7fd489810b37bd75ff117cfc496e0219b1439595
genbuos <swiftypython18@gmail.com>
2024-09-12 03:40:26 -0400

    first commit

---
+++ Algorithms/ADT/Pokemon.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.ADT
+{
+    public class Pokemon
+    {
+        public String Name { get; set;}
+        public int Exp { get; set;}
+
+        public bool IsPokemon()
+        {
+            return true;
+        }
+    }
+}
